b4dc2f045cc865118151aa95da36e66b5a251cde
ecithy <598779784@qq.com>
2022-03-17 12:15:01 +0800

    加入-路由系统

--- manage.py
+++ manage.py
@@ -2,14 +2,39 @@ from wsgiref.simple_server import make_server
 import settings
 
 
+def index():
+    return '<h1>Welcome to mfserver<h1>'.encode('utf-8')
+
+
+def error():
+    return '<h1>404 NOT FOUND<h1>'.encode('utf-8')
+
+
+urls = [
+    ('/', index),
+]
+
+
 # 函数对http请求与响应的封装、使得Python专注与HTML
 # environ http 请求 (dist)
 # start_response 响应 (function)
-def app(environ, start_response):
+def app(env, response):
     status = '200 OK'
     headers = [('Content-Type', 'text/html')]
-    start_response(status, headers)
-    return ['<h1>Hello, web!</h1>'.encode('utf-8')]
+    response(status, headers)
+
+    path = env['PATH_INFO']  # 拿到请求体中的路由
+    func = None
+    for url in urls:
+        if url[0] == path:
+            func = url[1]
+            break
+    if func:
+        response = func()
+    else:
+        response = error()
+
+    return [response]
 
 
 if __name__ == '__main__':
