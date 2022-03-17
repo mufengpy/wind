b72a2ba17d96b35d8da8adfb3922a0638f80f149
ecithy <598779784@qq.com>
2022-03-17 12:26:35 +0800

    路由+视图模块

---
+++ app01/urls.py
@@ -0,0 +1,5 @@
+from app01 import views
+
+urls = [
+    ('/', views.index),
+]
---
+++ app01/views.py
@@ -0,0 +1,6 @@
+def index():
+    return '<h1>Welcome to mfserver<h1>'.encode('utf-8')
+
+
+def error():
+    return '<h1>404 NOT FOUND<h1>'.encode('utf-8')
